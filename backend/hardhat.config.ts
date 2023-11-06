f0ac342590c667214aa0a0817be0a2144af99fb4
Ohio-User <149995219+Ohio-User@users.noreply.github.com>
2023-11-06 05:58:04 +0000

    test(codespaces): test codespace from diff account

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("dotenv").config();
+
+const URL = process.env.GOERLI_URL!;
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia : {
+      url: URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
