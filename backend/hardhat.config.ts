7ed0bafb0f4615df597217c57e41ff336f661f59
Deepanshu <deepanshu.r.sharma@gmail.com>
2023-11-06 23:14:26 +0530

    config hardhat

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("dotenv").config();
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia : {
+      url: URL,
+      accounts: [PRIVATE_KEY!],
+    },
+  }
 };
 
 export default config;
